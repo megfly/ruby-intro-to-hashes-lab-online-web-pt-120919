9fb4abecc3e5c37b348898e903553d68b2cff78b
Meghan Flynn <mflynn333@gmail.com>
2019-12-19 18:15:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
 	    :names => {
 	      :reading_railroad => {}, 
 	      :pennsylvania_railroad =>{}, 
-	      :b_and_o_railroad => {"mortgage_value" => '$400'}, 
+	      :b_and_o_railroad => {}, 
 	      :shortline => {"mortgage_value" => '$800'}
 	    },
 	    :rent_in_dollars => {
